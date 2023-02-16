b5980fe80ce9679d5264a7ac54e056f2439e2c52
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 11:51:34 +0530

    Initialize feature/impersoation-fix-logreader

---
+++ cdap-app-templates/cdap-etl/hydrator-spark-core/src/main/java/co/cask/cdap/etl/spark/streaming/DefaultStreamingContext.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.etl.spark.streaming;
+
+import co.cask.cdap.api.TxRunnable;
+import co.cask.cdap.api.spark.JavaSparkExecutionContext;
+import co.cask.cdap.etl.api.streaming.StreamingContext;
+import co.cask.cdap.etl.common.AbstractStageContext;
+import co.cask.cdap.etl.planner.StageInfo;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.tephra.TransactionFailureException;
+
+/**
+ * Default implementation of StreamingContext for Spark.
+ */
+public class DefaultStreamingContext extends AbstractStageContext implements StreamingContext {
+  private final JavaSparkExecutionContext sec;
+  private final JavaStreamingContext jsc;
+
+  public DefaultStreamingContext(StageInfo stageInfo, JavaSparkExecutionContext sec, JavaStreamingContext jsc) {
+    super(sec.getPluginContext(), sec.getMetrics(), stageInfo);
+    this.sec = sec;
+    this.jsc = jsc;
+  }
+
+  @Override
+  public JavaStreamingContext getSparkStreamingContext() {
+    return jsc;
+  }
+
+  @Override
+  public JavaSparkExecutionContext getSparkExecutionContext() {
+    return sec;
+  }
+
+  @Override
+  public void execute(TxRunnable runnable) throws TransactionFailureException {
+    sec.execute(runnable);
+  }
+
+  @Override
+  public void execute(int timeout, TxRunnable runnable) throws TransactionFailureException {
+    sec.execute(timeout, runnable);
+  }
+}
---
+++ cdap-docs/_common/_source/table-of-contents.rst
@@ -0,0 +1,22 @@
+.. meta::
+    :author: Cask Data, Inc.
+    :copyright: Copyright © 2014-2017 Cask Data, Inc.
+
+:hide-relations: true
+
+:hide-global-toc: true
+
+====================================
+CDAP Documentation Table of Contents
+====================================
+
+.. toctree::
+
+    Overview </index>
+    Introduction to CDAP </introduction/index>
+    Developers’ Manual </developers-manual/index>
+    Administration Manual </admin-manual/index>
+    Integrations </integrations/index>
+    Examples, Guides, and Tutorials </examples-manual/index>
+    Reference Manual </reference-manual/index>
+    FAQs </faqs/index>
