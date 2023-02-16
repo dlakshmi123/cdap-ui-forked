718ebab048e09a8a588e5ce3c417242c9cb417f2
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 11:52:09 +0530

    Initialize feature/kafka-perf-test

---
+++ cdap-app-templates/cdap-etl/cdap-etl-core/src/main/java/co/cask/cdap/etl/batch/mapreduce/MapReduceBatchContext.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.etl.batch.mapreduce;
+
+import co.cask.cdap.api.mapreduce.MapReduceContext;
+import co.cask.cdap.api.metrics.Metrics;
+import co.cask.cdap.etl.api.LookupProvider;
+import co.cask.cdap.etl.api.batch.BatchContext;
+import co.cask.cdap.etl.batch.AbstractBatchContext;
+import co.cask.cdap.etl.log.LogContext;
+import co.cask.cdap.etl.planner.StageInfo;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * Abstract implementation of {@link BatchContext} using {@link MapReduceContext}.
+ */
+public class MapReduceBatchContext extends AbstractBatchContext {
+  protected final MapReduceContext mrContext;
+
+  public MapReduceBatchContext(MapReduceContext context, Metrics metrics,
+                               LookupProvider lookup, Map<String, String> runtimeArguments,
+                               StageInfo stageInfo) {
+    super(context, metrics, lookup, context.getLogicalStartTime(), runtimeArguments, context.getAdmin(), stageInfo);
+    this.mrContext = context;
+  }
+
+  @Override
+  public <T> T getHadoopJob() {
+    return LogContext.runWithoutLoggingUnchecked(new Callable<T>() {
+      @Override
+      public T call() throws Exception {
+        return mrContext.getHadoopJob();
+      }
+    });
+  }
+}
---
+++ cdap-ui/app/cdap/components/EntityListView/EntityListInfo/index.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React, {PropTypes, Component} from 'react';
+import T from 'i18n-react';
+import ReactPaginate from 'react-paginate';
+
+require('./EntityListInfo.scss');
+
+export default class EntityListInfo extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handlePageChange(data) {
+    let clickedIndex = data.selected+1;
+    this.props.onPageChange(clickedIndex);
+  }
+
+  showPagination() {
+    let plus = this.props.allEntitiesFetched ? '' : '+';
+    let entitiesLabel = T.translate('features.EntityListView.Info.entities');
+    return (
+      <span className="pagination">
+        <span className="total-entities">
+          {this.props.numberOfEntities} {plus} {entitiesLabel}
+        </span>
+        {
+          this.props.numberOfPages > 1 ?
+            (
+              <ReactPaginate
+                pageCount={this.props.numberOfPages}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={1}
+                breakLabel={<span>...</span>}
+                breakClassName={"ellipsis"}
+                previousLabel={<span className="fa fa-angle-left"></span>}
+                nextLabel={<span className="fa fa-angle-right"></span>}
+                onPageChange={this.handlePageChange.bind(this)}
+                disableInitialCallback={true}
+                initialPage={this.props.currentPage-1}
+                forcePage={this.props.currentPage-1}
+                containerClassName={"page-list"}
+                activeClassName={"current-page"}
+              />
+            )
+          :
+            null
+        }
+      </span>
+    );
+  }
+  render() {
+    let title = T.translate('features.EntityListView.Info.title', {namespace: this.props.namespace});
+
+    return (
+      <div className={this.props.className}>
+        <span className="title">
+          <h3>{title}</h3>
+        </span>
+        {
+          this.props.numberOfEntities ?
+            this.showPagination()
+          :
+            null
+        }
+      </div>
+    );
+  }
+}
+
+EntityListInfo.propTypes = {
+  className: PropTypes.string,
+  namespace: PropTypes.string,
+  numberOfPages: PropTypes.number,
+  numberOfEntities: PropTypes.number,
+  currentPage: PropTypes.number,
+  onPageChange: PropTypes.func,
+  allEntitiesFetched: PropTypes.bool
+};
+
+EntityListInfo.defaultProps = {
+  className: '',
+  namespace: '',
+  numberOfPages: 1,
+  numberOfEntities: 0,
+  currentPage: 1,
+  onPageChange: () => {},
+  allEntitiesFetched: false
+};
