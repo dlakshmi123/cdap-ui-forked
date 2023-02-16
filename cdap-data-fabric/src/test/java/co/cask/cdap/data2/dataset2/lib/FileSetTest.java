c098084c83c191032e99b807d0e1797204633064
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 11:24:17 +0530

    Initialize feature_release/Add-stage-to-figure-out-real-datasets-for-preview

---
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/dataset2/lib/FileSetTest.java
@@ -0,0 +1,297 @@
+/*
+ * Copyright © 2014-2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.dataset2.lib;
+
+import co.cask.cdap.api.dataset.DataSetException;
+import co.cask.cdap.api.dataset.DatasetManagementException;
+import co.cask.cdap.api.dataset.lib.FileSet;
+import co.cask.cdap.api.dataset.lib.FileSetArguments;
+import co.cask.cdap.api.dataset.lib.FileSetProperties;
+import co.cask.cdap.common.conf.Constants;
+import co.cask.cdap.common.io.Locations;
+import co.cask.cdap.data2.dataset2.DatasetFrameworkTestUtil;
+import co.cask.cdap.data2.dataset2.lib.file.FileSetDataset;
+import co.cask.cdap.proto.id.DatasetId;
+import co.cask.cdap.proto.id.NamespaceId;
+import com.google.common.collect.Maps;
+import org.apache.tephra.TransactionFailureException;
+import org.apache.twill.filesystem.Location;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+public class FileSetTest {
+
+  @ClassRule
+  public static TemporaryFolder tmpFolder = new TemporaryFolder();
+
+  @ClassRule
+  public static DatasetFrameworkTestUtil dsFrameworkUtil = new DatasetFrameworkTestUtil();
+
+  static FileSet fileSet1;
+  static FileSet fileSet2;
+  private static final NamespaceId OTHER_NAMESPACE = new NamespaceId("yourspace");
+  private static final DatasetId testFileSetInstance1 =
+    DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("testFileSet");
+  private static final DatasetId testFileSetInstance2 = OTHER_NAMESPACE.dataset("testFileSet");
+  private static final DatasetId testFileSetInstance3 =
+    DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("absoluteFileSet");
+  private static final DatasetId testFileSetInstance4 =
+    DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("lookAlikeFileSet");
+  private static final DatasetId testFileSetInstance5 =
+    DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("externalFileSet");
+  private static final DatasetId testFileSetInstance6 =
+    DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("nonExternalFileSet1");
+
+  @Before
+  public void before() throws Exception {
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance1, FileSetProperties.builder()
+      .setBasePath("testDir").build());
+    Map<String, String> fileArgs = Maps.newHashMap();
+    FileSetArguments.setInputPath(fileArgs, "some?File1");
+    FileSetArguments.setOutputPath(fileArgs, "some?File1");
+    fileSet1 = dsFrameworkUtil.getInstance(testFileSetInstance1, fileArgs);
+
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance2, FileSetProperties.builder()
+      .setBasePath("testDir").build());
+    fileArgs = Maps.newHashMap();
+    FileSetArguments.setInputPath(fileArgs, "some?File2");
+    FileSetArguments.setOutputPath(fileArgs, "some?File2");
+    fileSet2 = dsFrameworkUtil.getInstance(testFileSetInstance2, fileArgs);
+  }
+
+  @After
+  public void after() throws Exception {
+    deleteInstance(testFileSetInstance1);
+    deleteInstance(testFileSetInstance2);
+    deleteInstance(testFileSetInstance3);
+    deleteInstance(testFileSetInstance4);
+    deleteInstance(testFileSetInstance5);
+    deleteInstance(testFileSetInstance6);
+  }
+
+  static void deleteInstance(DatasetId id) throws Exception {
+    if (dsFrameworkUtil.getInstance(id) != null) {
+      dsFrameworkUtil.deleteInstance(id);
+    }
+  }
+
+  @Test
+  public void testWriteRead() throws IOException {
+    Location fileSet1Output = fileSet1.getOutputLocation();
+    Location fileSet2Output = fileSet2.getOutputLocation();
+    Location fileSet1NsDir = Locations.getParent(Locations.getParent(Locations.getParent(fileSet1Output)));
+    Location fileSet2NsDir = Locations.getParent(Locations.getParent(Locations.getParent(fileSet2Output)));
+    Assert.assertNotNull(fileSet1NsDir);
+    Assert.assertNotNull(fileSet2NsDir);
+    Assert.assertEquals(fileSet1NsDir.getName(), DatasetFrameworkTestUtil.NAMESPACE_ID.getNamespace());
+    Assert.assertEquals(fileSet2NsDir.getName(), OTHER_NAMESPACE.getNamespace());
+
+    Assert.assertNotEquals(fileSet1.getInputLocations().get(0).toURI().getPath(),
+                           fileSet2.getInputLocations().get(0).toURI().getPath());
+    Assert.assertNotEquals(fileSet1Output.toURI().getPath(), fileSet2Output.toURI().getPath());
+
+    try (OutputStream out = fileSet1.getOutputLocation().getOutputStream()) {
+      out.write(42);
+    }
+    try (OutputStream out = fileSet2.getOutputLocation().getOutputStream()) {
+      out.write(54);
+    }
+
+    try (InputStream in = fileSet1.getInputLocations().get(0).getInputStream()) {
+      Assert.assertEquals(42, in.read());
+    }
+    try (InputStream in = fileSet2.getInputLocations().get(0).getInputStream()) {
+      Assert.assertEquals(54, in.read());
+    }
+  }
+
+  @Test
+  public void testAbsolutePath() throws IOException, DatasetManagementException {
+    String absolutePath = tmpFolder.newFolder() + "/absolute/path";
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance3, FileSetProperties.builder()
+      .setBasePath(absolutePath).build());
+    // validate that the base path for the file set was created
+    Assert.assertTrue(new File(absolutePath).isDirectory());
+
+    // instantiate the file set with an output path
+    Map<String, String> fileArgs = Maps.newHashMap();
+    FileSetArguments.setOutputPath(fileArgs, "out");
+    FileSet fileSet = dsFrameworkUtil.getInstance(testFileSetInstance3, fileArgs);
+
+    // write to the output path
+    Assert.assertEquals(absolutePath + "/out", fileSet.getOutputLocation().toURI().getPath());
+    try (OutputStream out = fileSet.getOutputLocation().getOutputStream()) {
+      out.write(42);
+    }
+
+    // validate that the file was created
+    Assert.assertTrue(new File(absolutePath + "/out").isFile());
+  }
+
+  @Test(expected = DataSetException.class)
+  public void testAbsolutePathInsideCDAP() throws IOException, DatasetManagementException {
+    String absolutePath = dsFrameworkUtil.getConfiguration().get(Constants.CFG_LOCAL_DATA_DIR).concat("/hello");
+    dsFrameworkUtil.createInstance("fileSet",
+                                   DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("badFileSet"),
+                                   FileSetProperties.builder().setBasePath(absolutePath).build());
+  }
+
+  @Test(expected = DataSetException.class)
+  public void testAbsolutePathInsideCDAPDouble() throws IOException, DatasetManagementException {
+    // test that it rejects also paths that have // in them
+    String absolutePath = dsFrameworkUtil.getConfiguration()
+      .get(Constants.CFG_LOCAL_DATA_DIR).replace("/", "//").concat("/hello");
+    dsFrameworkUtil.createInstance("fileSet",
+                                   DatasetFrameworkTestUtil.NAMESPACE_ID.dataset("badFileSet"),
+                                   FileSetProperties.builder().setBasePath(absolutePath).build());
+  }
+
+  @Test
+  public void testAbsolutePathLooksLikeCDAP() throws IOException, DatasetManagementException {
+    String absolutePath = dsFrameworkUtil.getConfiguration().get(Constants.CFG_LOCAL_DATA_DIR).concat("-hello");
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance4,
+                                   FileSetProperties.builder().setBasePath(absolutePath).build());
+  }
+
+  @Test
+  public void testExternalAbsolutePath() throws IOException, DatasetManagementException {
+    // create an external dir and create a file in it
+    String absolutePath = tmpFolder.newFolder() + "/absolute/path";
+    File absoluteFile = new File(absolutePath);
+    absoluteFile.mkdirs();
+    File someFile = new File(absoluteFile, "some.file");
+    someFile.createNewFile();
+
+    // create an external dataset
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance5,
+                                   FileSetProperties.builder()
+                                     .setBasePath(absolutePath)
+                                     .setDataExternal(true)
+                                     .build());
+
+    // instantiate the file set with an input and output path
+    Map<String, String> fileArgs = Maps.newHashMap();
+    FileSetArguments.setInputPath(fileArgs, "some.file");
+    FileSetArguments.setOutputPath(fileArgs, "out");
+    FileSet fileSet = dsFrameworkUtil.getInstance(testFileSetInstance5, fileArgs);
+    Assert.assertNotNull(fileSet);
+
+    // read the existing file
+    Location input = fileSet.getInputLocations().iterator().next();
+    InputStream in = input.getInputStream();
+    in.close();
+
+    // attempt to write an output file
+    try {
+      fileSet.getOutputLocation();
+      Assert.fail("Extrernal file set should not allow writing output.");
+    } catch (UnsupportedOperationException e) {
+      // expected
+    }
+
+    // delete the dataset and validate that the files are still there
+    dsFrameworkUtil.deleteInstance(testFileSetInstance5);
+    Assert.assertTrue(someFile.exists());
+  }
+
+  @Test(expected = IOException.class)
+  public void testExternalNonExistentPath() throws IOException, DatasetManagementException {
+    // create an external dir and create a file in it
+    String absolutePath = tmpFolder.newFolder() + "/not/there";
+    // attempt to create an external dataset - should fail
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance5,
+                                   FileSetProperties.builder()
+                                     .setBasePath(absolutePath)
+                                     .setDataExternal(true)
+                                     .build());
+  }
+
+
+  @Test(expected = IOException.class)
+  public void testNonExternalExistentPath() throws Exception {
+    // Create an instance at a location
+    String absolutePath = tmpFolder.newFolder() + "/some/existing/location";
+    File file = new File(absolutePath);
+    Assert.assertTrue(file.mkdirs());
+    // Try to add another instance of non external fileset at the same location
+    dsFrameworkUtil.createInstance("fileSet", testFileSetInstance6,
+                                   FileSetProperties.builder()
+                                     .setBasePath(absolutePath)
+                                     .setDataExternal(false)
+                                     .build());
+  }
+
+  @Test
+   public void testRollback() throws IOException, TransactionFailureException {
+    // test deletion of an empty output directory
+    Location outputLocation = fileSet1.getOutputLocation();
+    Assert.assertFalse(outputLocation.exists());
+
+    Assert.assertTrue(outputLocation.mkdirs());
+
+    Assert.assertTrue(outputLocation.exists());
+    ((FileSetDataset) fileSet1).onFailure();
+
+    Assert.assertFalse(outputLocation.exists());
+  }
+
+  @Test
+  public void testRollbackOfNonDirectoryOutput() throws IOException, TransactionFailureException {
+    // test deletion of an output location, pointing to a non-directory file
+    Location outputFile = fileSet1.getOutputLocation();
+    Assert.assertFalse(outputFile.exists());
+
+    outputFile.getOutputStream().close();
+
+    Assert.assertTrue(outputFile.exists());
+    ((FileSetDataset) fileSet1).onFailure();
+
+    // the output file should still not be deleted
+    Assert.assertTrue(outputFile.exists());
+  }
+
+  @Test
+  public void testRollbackWithNonEmptyDir() throws IOException, TransactionFailureException {
+    Location outputDir = fileSet1.getOutputLocation();
+    Assert.assertFalse(outputDir.exists());
+
+    Assert.assertTrue(outputDir.mkdirs());
+
+    Location outputFile = outputDir.append("outputFile");
+    // this will create the outputFile
+    outputFile.getOutputStream().close();
+
+    Assert.assertTrue(outputFile.exists());
+
+    Assert.assertTrue(outputDir.exists());
+    ((FileSetDataset) fileSet1).onFailure();
+
+    // both the output dir and file in it should still exist
+    Assert.assertTrue(outputDir.exists());
+    Assert.assertTrue(outputFile.exists());
+  }
+}
---
+++ cdap-ui/app/wrangler/components/Wrangler/index.js
@@ -0,0 +1,408 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import Papa from 'papaparse';
+import WrangleData from 'wrangler/components/Wrangler/WrangleData';
+import WranglerActions from 'wrangler/components/Wrangler/Store/WranglerActions';
+import WranglerStore from 'wrangler/components/Wrangler/Store/WranglerStore';
+import shortid from 'shortid';
+import Dropzone from 'react-dropzone';
+import {convertHistoryToDml} from 'wrangler/components/Wrangler/dml-converter';
+import Explore from 'wrangler/components/Explore';
+
+require('./Wrangler.less');
+
+/**
+ * 3 Steps for any transforms:
+ *    1. Format the data
+ *    2. Handle the ordering of Headers
+ *    3. Handle the inferred type of the column
+ **/
+export default class Wrangler extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textarea: false,
+      loading: false,
+      header: false,
+      skipEmptyLines: false,
+      delimiter: '',
+      wranglerInput: '',
+      isDataSet: false,
+      file: '',
+      errors: []
+    };
+
+    this.handleSetHeaders = this.handleSetHeaders.bind(this);
+    this.handleSetSkipEmptyLines = this.handleSetSkipEmptyLines.bind(this);
+    this.setDelimiter = this.setDelimiter.bind(this);
+    this.wrangle = this.wrangle.bind(this);
+    this.handleData = this.handleData.bind(this);
+    this.handleTextInput = this.handleTextInput.bind(this);
+    this.onPlusButtonClick = this.onPlusButtonClick.bind(this);
+    this.onWrangleClick = this.onWrangleClick.bind(this);
+    this.getWranglerOutput = this.getWranglerOutput.bind(this);
+    this.onTextInputBlur = this.onTextInputBlur.bind(this);
+  }
+
+  getChildContext() {
+    return {source: this.props.source};
+  }
+
+  componentWillUnmount() {
+    WranglerStore.dispatch({
+      type: WranglerActions.reset
+    });
+  }
+  onWrangleClick() {
+    let input = this.state.wranglerInput;
+
+    if (this.state.file) {
+      input = this.state.file;
+    }
+
+    this.wrangle(input, this.state.delimiter, this.state.header, this.state.skipEmptyLines);
+  }
+
+  wrangle(input, delimiter, header, skipEmptyLines) {
+    this.setState({loading: true});
+
+    let papaConfig = {
+      header: header,
+      skipEmptyLines: skipEmptyLines,
+      complete: this.handleData
+    };
+
+    if (delimiter) {
+      papaConfig.delimiter = delimiter;
+    }
+
+    Papa.parse(input, papaConfig);
+  }
+
+  onPlusButtonClick() {
+    this.setState({textarea: true});
+  }
+
+  handleData(papa) {
+    if (papa.errors.length > 0) {
+      this.setState({errors: papa.errors, loading: false});
+      return;
+    }
+
+    let formattedData;
+    if (Array.isArray(papa.data[0])) {
+      // Get length of the longest column
+      let numColumns = papa.data.reduce((prev, curr) => {
+        return prev > curr.length ? prev : curr.length;
+      }, 0);
+
+      formattedData = papa.data.map((row) => {
+        let obj = {};
+        for (let i = 0; i < numColumns; i++) {
+          let key = `column${i+1}`;
+          let value = row[i];
+
+          obj[key] = !value ? '' : value;
+        }
+
+        return obj;
+      });
+    } else {
+      formattedData = papa.data;
+    }
+
+    WranglerStore.dispatch({
+      type: WranglerActions.setData,
+      payload: {
+        data: formattedData,
+      }
+    });
+
+    this.setState({
+      isDataSet: true,
+      loading: false,
+      delimiter: papa.meta.delimiter
+    });
+  }
+
+  setDelimiter(e) {
+    this.setState({delimiter: e.target.value});
+  }
+
+  handleSetHeaders() {
+    this.setState({header: !this.state.header});
+  }
+  handleSetSkipEmptyLines() {
+    this.setState({skipEmptyLines: !this.state.skipEmptyLines});
+  }
+  handleTextInput(e) {
+    this.setState({wranglerInput: e.target.value});
+  }
+
+  renderErrors() {
+    if (this.state.errors.length === 0) {
+      return null;
+    }
+
+    return (
+      <div className="wrangler-error-container">
+        <h4 className="error text-center">Errors:</h4>
+        <table className="table table-bordered error-table">
+          <thead>
+            <tr>
+              <th>Row</th>
+              <th>Error</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.errors.map((error) => {
+                return (
+                  <tr key={shortid.generate()}>
+                    <td>{error.row}</td>
+                    <td>{error.message}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+
+  }
+
+  preventPropagation (e) {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+  }
+
+  renderWranglerCopyPaste() {
+    if (!this.state.textarea || this.state.file.name) {
+      return (
+        <div
+          className="wrangler-plus-button text-center"
+          onClick={this.onPlusButtonClick}
+        >
+          <div
+            className="dropzone-container"
+            onClick={(e) => this.preventPropagation(e)}
+          >
+            <Dropzone
+              activeClassName="wrangler-file-drag-container"
+              className="wrangler-file-drop-container"
+              onDrop={(e) => this.setState({file: e[0], textarea: false})}
+            >
+              {
+                this.state.file.name && this.state.file.name.length ?
+                  null
+                :
+                  (
+                    <div className="plus-button">
+                      <i className="fa fa-plus-circle"></i>
+                    </div>
+                  )
+              }
+            </Dropzone>
+          </div>
+
+          <div className="plus-button-helper-text">
+            {
+              this.state.file.name && this.state.file.name.length ?
+              (<h4>{this.state.file.name}</h4>)
+                :
+              (
+                <div>
+                  <h4>Click <span className="fa fa-plus-circle" /> to upload a file</h4>
+                  <h5>or</h5>
+                  <h4>Click anywhere else to copy paste data</h4>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <textarea
+        className="form-control"
+        onChange={this.handleTextInput}
+        autoFocus={true}
+        onBlur={this.onTextInputBlur}
+      />
+    );
+  }
+
+  onTextInputBlur() {
+    if (this.state.wranglerInput) { return; }
+
+    this.setState({textarea: false});
+  }
+
+  getWranglerOutput() {
+    let state = WranglerStore.getState().wrangler;
+
+    // prepare specifications
+    let dml = convertHistoryToDml(state.history.slice(0, state.historyLocation));
+
+    let setFormat = `set format csv ${this.state.delimiter} ${this.state.skipEmptyLines}`;
+    let initialColumns = state.initialHeaders.join(',');
+    let setColumns = `set columns ${initialColumns}`;
+
+    let spec = [setFormat, setColumns].concat(dml).join('\n');
+
+
+    // prepare schema
+    let fields = [];
+
+    state.headersList.forEach((column) => {
+      let hasError = state.errors[column] && state.errors[column].count > 0;
+
+      fields.push({
+        name: column,
+        type: hasError ? [state.columnTypes[column], 'null'] : state.columnTypes[column]
+      });
+    });
+
+    let schema = {
+      name: 'etlSchemaBody',
+      type: 'record',
+      fields
+    };
+
+    let properties = {
+      specification: spec,
+      schema: JSON.stringify(schema)
+    };
+
+    if (typeof this.props.hydrator === 'function') {
+      this.props.hydrator(properties);
+    }
+
+    return properties;
+  }
+
+  renderWranglerInputBox() {
+    if (this.state.isDataSet) {
+      return null;
+    }
+
+    if (this.state.loading) {
+      return (
+        <div className="loading text-center">
+          <div>
+            <span className="fa fa-spinner fa-spin"></span>
+          </div>
+          <h3>Parsing...</h3>
+        </div>
+      );
+    }
+
+    return (
+      <div className="wrangler-input-container">
+        <h3 className="text-center">Cask Wrangler</h3>
+
+        <Explore wrangle={this.wrangle} />
+
+        <div className="wrangler-copy-paste">
+          {this.renderWranglerCopyPaste()}
+
+          <div className="parse-options">
+            <form className="form-inline">
+              <div className="delimiter">
+                {/* delimiter */}
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Set delimiter"
+                  onChange={this.setDelimiter}
+                />
+              </div>
+
+              <hr/>
+
+              <div className="checkbox">
+                {/* header */}
+                <label>
+                  <input type="checkbox"
+                    onChange={this.handleSetHeaders}
+                    checked={this.state.headers}
+                  /> First line as column name
+                </label>
+              </div>
+
+              <div className="checkbox">
+                {/* skipEmptyLines */}
+                <label>
+                  <input type="checkbox"
+                    onChange={this.handleSetSkipEmptyLines}
+                  /> Skip empty lines
+                </label>
+              </div>
+            </form>
+          </div>
+
+        </div>
+
+        <br/>
+
+        <div className="text-center">
+          <button
+            className="btn btn-wrangler wrangle-button"
+            onClick={this.onWrangleClick}
+          >
+            Wrangle
+          </button>
+        </div>
+
+        {this.renderErrors()}
+
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="wrangler-container">
+
+        {this.renderWranglerInputBox()}
+
+        {
+          this.state.isDataSet ?
+            <WrangleData
+              onHydratorApply={this.getWranglerOutput}
+            />
+          :
+            null
+        }
+
+      </div>
+    );
+  }
+}
+
+Wrangler.propTypes = {
+  source: PropTypes.oneOf(['wrangler', 'hydrator']),
+  hydrator: PropTypes.any
+};
+
+Wrangler.childContextTypes = {
+  source: PropTypes.oneOf(['wrangler', 'hydrator'])
+};
