ef99f85ebb221e564fe4922f7dfba8737f6fd22d
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 11:52:44 +0530

    Initialize bugfix_release/tx-snapshot-ref-count

---
+++ cdap-data-fabric/src/main/java/co/cask/cdap/data2/transaction/queue/hbase/coprocessor/ConsumerConfigCache.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright © 2014 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.transaction.queue.hbase.coprocessor;
+
+import co.cask.cdap.common.conf.CConfiguration;
+import co.cask.cdap.data2.transaction.queue.QueueConstants;
+import co.cask.cdap.data2.transaction.queue.QueueEntryRow;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Supplier;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.common.io.InputSupplier;
+import com.google.common.util.concurrent.AbstractIdleService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.TableNotFoundException;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.tephra.Transaction;
+import org.apache.tephra.TransactionCodec;
+import org.apache.tephra.TxConstants;
+import org.apache.tephra.persist.TransactionSnapshot;
+import org.apache.tephra.persist.TransactionVisibilityState;
+import org.apache.tephra.util.TxUtils;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+
+/**
+ * Provides a RegionServer shared cache for all instances of {@code HBaseQueueRegionObserver} of the recent
+ * queue consumer configuration.
+ */
+public class ConsumerConfigCache extends AbstractIdleService {
+  private static final Log LOG = LogFactory.getLog(ConsumerConfigCache.class);
+
+  // Number of bytes for consumer state column (groupId + instanceId)
+  private static final int STATE_COLUMN_SIZE = Bytes.SIZEOF_LONG + Bytes.SIZEOF_INT;
+  // update interval for CConfiguration
+  private static final long CONFIG_UPDATE_FREQUENCY = 300 * 1000L;
+
+  private static final ConcurrentMap<TableName, ConsumerConfigCache> INSTANCES = new ConcurrentHashMap<>();
+
+  private final TableName queueConfigTableName;
+  private final CConfigurationReader cConfReader;
+  private final Supplier<TransactionVisibilityState> transactionSnapshotSupplier;
+  private final InputSupplier<HTableInterface> hTableSupplier;
+  private final TransactionCodec txCodec;
+
+  private Thread refreshThread;
+  private long lastUpdated;
+  private volatile Map<byte[], QueueConsumerConfig> configCache = Maps.newTreeMap(Bytes.BYTES_COMPARATOR);
+  private volatile boolean stopped;
+  private long configCacheUpdateFrequency = QueueConstants.DEFAULT_QUEUE_CONFIG_UPDATE_FREQUENCY;
+  private volatile CConfiguration conf;
+  // timestamp of the last update from the configuration table
+  private long lastConfigUpdate;
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param queueConfigTableName table name that stores queue configuration
+   * @param cConfReader reader to read the latest {@link CConfiguration}
+   * @param transactionSnapshotSupplier A supplier for the latest {@link TransactionSnapshot}
+   * @param hTableSupplier A supplier for creating {@link HTableInterface}.
+   */
+  ConsumerConfigCache(TableName queueConfigTableName, CConfigurationReader cConfReader,
+                      Supplier<TransactionVisibilityState> transactionSnapshotSupplier,
+                      InputSupplier<HTableInterface> hTableSupplier) {
+    this.queueConfigTableName = queueConfigTableName;
+    this.cConfReader = cConfReader;
+    this.transactionSnapshotSupplier = transactionSnapshotSupplier;
+    this.hTableSupplier = hTableSupplier;
+    this.txCodec = new TransactionCodec();
+  }
+
+  @Override
+  protected void startUp() throws Exception {
+    LOG.info("Starting ConsumerConfigCache service.");
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    LOG.info("Stopping ConsumerConfigCache service.");
+    stopped = true;
+    Set<ConsumerConfigCache> caches = ImmutableSet.copyOf(INSTANCES.values());
+    for (ConsumerConfigCache cache : caches) {
+      try {
+        cache.stopRefreshThread();
+      } catch (InterruptedException ex) {
+        LOG.warn(String.format("Stopping of refresh thread of ConsumerConfigCache reading from config table %s " +
+                                 "threw an exception", queueConfigTableName), ex);
+      }
+    }
+  }
+
+  private void init() {
+    startRefreshThread();
+  }
+
+  private void stopRefreshThread() throws InterruptedException {
+    if (refreshThread != null) {
+      refreshThread.interrupt();
+      refreshThread.join(TimeUnit.SECONDS.toMillis(1));
+    }
+  }
+
+  public boolean isAlive() {
+    return refreshThread.isAlive();
+  }
+
+  @Nullable
+  public CConfiguration getCConf() {
+    return (conf != null) ? CConfiguration.copy(conf) : null;
+  }
+
+  @Nullable
+  public QueueConsumerConfig getConsumerConfig(byte[] queueName) {
+    return configCache.get(queueName);
+  }
+
+  private void updateConfig() {
+    long now = System.currentTimeMillis();
+    if (this.conf == null || now > (lastConfigUpdate + CONFIG_UPDATE_FREQUENCY)) {
+      try {
+        CConfiguration conf = cConfReader.read();
+        if (conf != null) {
+          this.conf = conf;
+          LOG.info(String.format("Reloaded CConfiguration at %d", now));
+          this.lastConfigUpdate = now;
+          long configUpdateFrequency = conf.getLong(QueueConstants.QUEUE_CONFIG_UPDATE_FREQUENCY,
+                                                    QueueConstants.DEFAULT_QUEUE_CONFIG_UPDATE_FREQUENCY);
+          LOG.info(String.format("Will reload consumer config cache every %d seconds", configUpdateFrequency));
+          this.configCacheUpdateFrequency = configUpdateFrequency * 1000;
+        }
+      } catch (IOException ioe) {
+        LOG.error("Error reading default configuration table", ioe);
+      }
+    }
+  }
+
+  /**
+   * This forces an immediate update of the config cache. It should only be called from the refresh thread or from
+   * tests, to avoid having to add a sleep for the duration of the refresh interval.
+   *
+   * This method is synchronized to protect from race conditions if called directly from a test. Otherwise this is
+   * only called from the refresh thread, and there will not be concurrent invocations.
+   *
+   * @throws IOException if failed to update config cache
+   */
+  @VisibleForTesting
+  public synchronized void updateCache() throws IOException {
+    Map<byte[], QueueConsumerConfig> newCache = Maps.newTreeMap(Bytes.BYTES_COMPARATOR);
+    long now = System.currentTimeMillis();
+    TransactionVisibilityState txSnapshot = transactionSnapshotSupplier.get();
+    if (txSnapshot == null) {
+      LOG.debug("No transaction snapshot is available. Not updating the consumer config cache.");
+      return;
+    }
+
+    HTableInterface table = hTableSupplier.getInput();
+    try {
+      // Scan the table with the transaction snapshot
+      Scan scan = new Scan();
+      scan.addFamily(QueueEntryRow.COLUMN_FAMILY);
+      Transaction tx = TxUtils.createDummyTransaction(txSnapshot);
+      setScanAttribute(scan, TxConstants.TX_OPERATION_ATTRIBUTE_KEY, txCodec.encode(tx));
+      ResultScanner scanner = table.getScanner(scan);
+      int configCnt = 0;
+      for (Result result : scanner) {
+        if (!result.isEmpty()) {
+          NavigableMap<byte[], byte[]> familyMap = result.getFamilyMap(QueueEntryRow.COLUMN_FAMILY);
+          if (familyMap != null) {
+            configCnt++;
+            Map<ConsumerInstance, byte[]> consumerInstances = new HashMap<>();
+            // Gather the startRow of all instances across all consumer groups.
+            int numGroups = 0;
+            Long groupId = null;
+            for (Map.Entry<byte[], byte[]> entry : familyMap.entrySet()) {
+              if (entry.getKey().length != STATE_COLUMN_SIZE) {
+                continue;
+              }
+              long gid = Bytes.toLong(entry.getKey());
+              int instanceId = Bytes.toInt(entry.getKey(), Bytes.SIZEOF_LONG);
+              consumerInstances.put(new ConsumerInstance(gid, instanceId), entry.getValue());
+
+              // Columns are sorted by groupId, hence if it change, then numGroups would get +1
+              if (groupId == null || groupId != gid) {
+                numGroups++;
+                groupId = gid;
+              }
+            }
+            byte[] queueName = result.getRow();
+            newCache.put(queueName, new QueueConsumerConfig(consumerInstances, numGroups));
+          }
+        }
+      }
+      long elapsed = System.currentTimeMillis() - now;
+      this.configCache = newCache;
+      this.lastUpdated = now;
+      if (LOG.isDebugEnabled()) {
+        LOG.debug(String.format("Updated consumer config cache with %d entries, took %d msec", configCnt, elapsed));
+      }
+    } finally {
+      try {
+        table.close();
+      } catch (IOException ioe) {
+        LOG.error(String.format("Error closing table %s", queueConfigTableName), ioe);
+      }
+    }
+  }
+
+  private void startRefreshThread() {
+    refreshThread = new Thread("queue-cache-refresh") {
+      @Override
+      public void run() {
+        while ((!isInterrupted()) && !stopped) {
+          updateConfig();
+          long now = System.currentTimeMillis();
+          if (now > (lastUpdated + configCacheUpdateFrequency)) {
+            try {
+              updateCache();
+            } catch (TableNotFoundException e) {
+              // This is expected when the namespace goes away since there is one config table per namespace
+              // If the table is not found due to other situation, the region observer already
+              // has logic to get a new one through the getInstance method
+              LOG.warn(String.format("Queue config table not found: %s", queueConfigTableName), e);
+              break;
+            } catch (IOException e) {
+              LOG.warn("Error updating queue consumer config cache", e);
+            }
+          }
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException ie) {
+            // reset status
+            interrupt();
+            break;
+          }
+        }
+        LOG.info(String.format("Config cache update for %s terminated.", queueConfigTableName));
+        INSTANCES.remove(queueConfigTableName, this);
+      }
+    };
+    refreshThread.setDaemon(true);
+    refreshThread.start();
+  }
+
+  /**
+   * Sets an attribute to the given {@link Scan} object. Instead of calling {@link Scan#setAttribute(String, byte[])}
+   * directly, it uses reflection to call the method. This is because the return type for the setAttribute method
+   * is different in different HBase version.
+   */
+  private void setScanAttribute(Scan scan, String name, byte[] value) {
+    try {
+      Method setAttribute = scan.getClass().getMethod("setAttribute", String.class, byte[].class);
+      setAttribute.invoke(scan, name, value);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      LOG.error("Failed to call Scan.setAttribute", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  public static ConsumerConfigCache getInstance(TableName tableName,
+                                                CConfigurationReader cConfReader,
+                                                Supplier<TransactionVisibilityState> txSnapshotSupplier,
+                                                InputSupplier<HTableInterface> hTableSupplier) {
+    ConsumerConfigCache cache = INSTANCES.get(tableName);
+    if (cache == null) {
+      cache = new ConsumerConfigCache(tableName, cConfReader, txSnapshotSupplier, hTableSupplier);
+      if (INSTANCES.putIfAbsent(tableName, cache) == null) {
+        // if another thread created an instance for the same table, that's ok, we only init the one saved
+        cache.init();
+      } else {
+        // discard our instance and re-retrieve, someone else set it
+        cache = INSTANCES.get(tableName);
+      }
+    }
+    return cache;
+  }
+}
---
+++ cdap-docs/admin-manual/source/installation/index.rst
@@ -0,0 +1,69 @@
+.. meta::
+    :author: Cask Data, Inc.
+    :copyright: Copyright © 2014-2017 Cask Data, Inc.
+
+:hide-toc: true
+
+.. _installation-index:
+
+============
+Installation
+============
+
+.. toctree::
+   
+    Cloudera Manager <cloudera>
+    Amazon EMR <emr>
+    Apache Ambari <ambari>
+    MapR <mapr>
+    Packages <packages>
+
+.. these are redirects for deleted pages
+
+.. toctree::
+   :hidden:
+
+   cdap-site
+   installation
+   security
+
+    
+Installation and configuration instructions for either **specific distributions** or
+**generic Apache Hadoop** clusters using RPM or Debian Package Managers:
+
+- :ref:`Installation using Cloudera Manager <admin-installation-cloudera>`
+
+  - :ref:`Advanced Topics: <cloudera-installation-advanced-topics>`
+    enabling perimeter security, Kerberos, CDAP high availability, and CDAP Explore engines
+
+..
+
+- :ref:`Installation for Amazon EMR <admin-installation-emr>`
+
+  - **Advanced Topics** (enabling perimeter security, CDAP high availability, CDAP
+    Explore engines, and enabling Kerberos) are currently not available for Amazon EMR
+
+..
+
+- :ref:`Installation using Apache Ambari <admin-installation-ambari>`
+
+  - :ref:`Advanced Topics: <ambari-installation-advanced-topics>`
+    enabling perimeter security, Kerberos, CDAP high availability, and CDAP Explore engines
+
+..
+
+- :ref:`Installation for MapR <admin-installation-mapr>`
+
+  - :ref:`Advanced Topics: <mapr-installation-advanced-topics>`
+    enabling perimeter security, Kerberos, CDAP high availability, and CDAP Explore engines
+
+..
+
+- :ref:`Manual Installation using Packages <admin-installation-packages>`
+
+  - :ref:`Advanced Topics: <packages-installation-advanced-topics>`
+    enabling perimeter security, Kerberos, CDAP high availability, and CDAP Explore engines
+
+..
+
+For installation FAQs, see the :ref:`FAQs <faqs>`.
