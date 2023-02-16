a7174dbc72df13fc69eabb5e0443a91e8bf5ff72
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 11:40:56 +0530

    Initialize bugfix/cdap-7155-ds-bootstrap-timeout

---
+++ cdap-app-fabric/src/main/java/co/cask/cdap/operations/OperationalStatsService.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.operations;
+
+import co.cask.cdap.common.conf.CConfiguration;
+import co.cask.cdap.common.conf.Constants;
+import com.google.common.base.Throwables;
+import com.google.common.util.concurrent.AbstractScheduledService;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import org.apache.twill.common.Threads;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.management.ManagementFactory;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MXBean;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+/**
+ * A service that registers {@link OperationalStats} extensions as JMX Beans. The Beans are registered with the JMX
+ * domain {@link OperationalStatsUtils#JMX_DOMAIN} with the following properties:
+ * <ol>
+ *   <li>{@link OperationalStatsUtils#SERVICE_NAME_KEY} = name of the service, as defined by
+ *   {@link OperationalStats#getServiceName()};</li>
+ *   <li>{@link OperationalStatsUtils#STAT_TYPE_KEY} = type of the stat; as defined by
+ *   {@link OperationalStats#getStatType()}</li>
+ * </ol>
+ *
+ * It also updates the Beans periodically by calling their {@link OperationalStats#collect()} method.
+ */
+public class OperationalStatsService extends AbstractScheduledService {
+  private static final Logger LOG = LoggerFactory.getLogger(OperationalStatsService.class);
+
+  private final OperationalStatsLoader operationalStatsLoader;
+  private final int statsRefreshInterval;
+  private final Injector injector;
+
+  private ScheduledExecutorService executor;
+
+  @Inject
+  OperationalStatsService(OperationalStatsLoader operationalStatsLoader, CConfiguration cConf, Injector injector) {
+    this.operationalStatsLoader = operationalStatsLoader;
+    this.statsRefreshInterval = cConf.getInt(Constants.OperationalStats.REFRESH_INTERVAL_SECS);
+    this.injector = injector;
+  }
+
+  /**
+   * Registers all JMX {@link MXBean MXBeans} from {@link OperationalStats} extensions in the extensions directory.
+   */
+  @Override
+  protected void startUp() throws Exception {
+    MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+    for (Map.Entry<OperationalExtensionId, OperationalStats> entry : operationalStatsLoader.getAll().entrySet()) {
+      OperationalStats operationalStats = entry.getValue();
+      ObjectName objectName = getObjectName(operationalStats);
+      if (objectName == null) {
+        LOG.warn("Found an operational extension with null service name and stat type - {}. Ignoring this extension.",
+                 OperationalStats.class.getName());
+        continue;
+      }
+      LOG.debug("Registering operational extension: {}; extension id: {}", operationalStats, entry.getKey());
+      // initialize operational stats
+      operationalStats.initialize(injector);
+      // register MBean
+      mbs.registerMBean(operationalStats, objectName);
+    }
+    LOG.info("Successfully started Operational Stats Service...");
+  }
+
+  /**
+   * Also schedules asynchronous stats collection for all {@link MXBean MXBeans} by calling the
+   * {@link OperationalStats#collect()} method.
+   */
+  @Override
+  protected void runOneIteration() throws Exception {
+    LOG.debug("Running operational stats extension service iteration");
+    for (Map.Entry<OperationalExtensionId, OperationalStats> entry : operationalStatsLoader.getAll().entrySet()) {
+      LOG.debug("Collecting {} stats for service {}", entry.getValue().getStatType(),
+                entry.getValue().getServiceName());
+      try {
+        entry.getValue().collect();
+      } catch (Throwable t) {
+        LOG.warn("Error while collecting stats for service: {}; type: {}", entry.getValue().getServiceName(),
+                 entry.getValue().getStatType(), t);
+      }
+    }
+  }
+
+  @Override
+  protected Scheduler scheduler() {
+    return Scheduler.newFixedDelaySchedule(0, statsRefreshInterval, TimeUnit.SECONDS);
+  }
+
+  @Override
+  protected ScheduledExecutorService executor() {
+    executor = Executors.newSingleThreadScheduledExecutor(
+      Threads.createDaemonThreadFactory("operational-stats-collector-%d"));
+    return executor;
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+    for (Map.Entry<OperationalExtensionId, OperationalStats> entry : operationalStatsLoader.getAll().entrySet()) {
+      OperationalStats operationalStats = entry.getValue();
+      ObjectName objectName = getObjectName(operationalStats);
+      if (objectName == null) {
+        LOG.warn("Found an operational extension with null service name and stat type while unregistering - {}. " +
+                   "Ignoring this extension.", operationalStats.getClass().getName());
+        continue;
+      }
+      try {
+        mbs.unregisterMBean(objectName);
+      } catch (InstanceNotFoundException e) {
+        LOG.debug("MBean {} not found while un-registering. Ignoring.", objectName);
+      } catch (MBeanRegistrationException e) {
+        LOG.warn("Error while un-registering MBean {}.", e);
+      }
+      operationalStats.destroy();
+    }
+    if (executor != null) {
+      executor.shutdownNow();
+    }
+    LOG.info("Successfully shutdown operational stats service.");
+  }
+
+  @Nullable
+  private ObjectName getObjectName(OperationalStats operationalStats) {
+    OperationalExtensionId operationalExtensionId = OperationalStatsUtils.getOperationalExtensionId(operationalStats);
+    if (operationalExtensionId == null) {
+      return null;
+    }
+    Hashtable<String, String> properties = new Hashtable<>();
+    properties.put(OperationalStatsUtils.SERVICE_NAME_KEY, operationalExtensionId.getServiceName());
+    properties.put(OperationalStatsUtils.STAT_TYPE_KEY, operationalExtensionId.getStatType());
+    try {
+      return new ObjectName(OperationalStatsUtils.JMX_DOMAIN, properties);
+    } catch (MalformedObjectNameException e) {
+      // should never happen, since we're constructing a valid domain name, and properties is non-empty
+      throw Throwables.propagate(e);
+    }
+  }
+}
