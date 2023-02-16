1b05b2a234e80405b3d9185ab20b2d925a9fe63c
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 10:19:54 +0000

    Initialize feature/docker-services

---
+++ cdap-ui/app/cdap/components/CopyableID/index.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
+import {UncontrolledTooltip} from 'components/UncontrolledComponents';
+import Clipboard from 'clipboard';
+import IconSVG from 'components/IconSVG';
+import T from 'i18n-react';
+
+require('./CopyableID.scss');
+
+const PREFIX = `features.CopyableID`;
+
+export default class CopyableID extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    idprefix: PropTypes.string,
+    label: PropTypes.string
+  };
+
+  static defaultProps = {
+    label: T.translate(`${PREFIX}.label`)
+  };
+
+  state = {
+    showTooltip: false
+  };
+
+  onIDClickHandler() {
+    this.setState({
+      showTooltip: !this.state.showTooltip
+    });
+  }
+  render() {
+    let idlabel = `A-${this.props.id}`;
+    if (this.props.idprefix) {
+      idlabel = `${this.props.idprefix}-${this.props.id}`;
+    }
+    // FIXME: Not sure how else to do this. Looks adhoc. Need this for copy to clipboard.
+    new Clipboard(`#${idlabel}`);
+    let tetherConfig = {
+      classPrefix: 'copyable-id-tooltip'
+    };
+    let tooltipProps = {
+      target: idlabel,
+      placement: 'right',
+      tether: tetherConfig,
+      delay: 0
+    };
+    if (this.state.showTooltip) {
+      tooltipProps.isOpen = true;
+    }
+    return (
+      <span
+        className="btn-link"
+        id={idlabel}
+        onClick={this.onIDClickHandler.bind(this, this.props.id)}
+        onMouseOut={() => {
+          this.state.showTooltip ? this.onIDClickHandler() : null;
+        }}
+        data-clipboard-text={this.props.id}
+      >
+        <span>{this.props.label}</span>
+        <UncontrolledTooltip
+          {...tooltipProps}
+        >
+          <span>{this.props.id || T.translate(`${PREFIX}.notAvailable`)}</span>
+          {
+            this.state.showTooltip ?
+              <span className="copied-label text-success">
+                <IconSVG name="icon-check-circle" />
+                <span>{T.translate(`${PREFIX}.copiedLabel`)}</span>
+              </span>
+            :
+              null
+          }
+        </UncontrolledTooltip>
+      </span>
+    );
+  }
+}
---
+++ cdap-ui/app/cdap/components/Home/index.js
@@ -0,0 +1,121 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Page404 from 'components/404';
+import EntityListView from 'components/EntityListView';
+import LoadingSVGCentered from 'components/LoadingSVGCentered';
+import Loadable from 'react-loadable';
+import NamespaceStore from 'services/NamespaceStore';
+import NamespaceActions from 'services/NamespaceStore/NamespaceActions';
+import {setActiveBrowser, setDatabaseProperties} from 'components/DataPrep/DataPrepBrowser/DataPrepBrowserStore/ActionCreator';
+require('./Home.scss');
+
+const DataPrepBrowser = Loadable({
+  loader: () => import(/* webpackChunkName: "DataPrepBrowser" */ 'components/DataPrep/DataPrepBrowser'),
+  loading: LoadingSVGCentered
+});
+const DataPrepConnections = Loadable({
+  loader: () => import(/* webpackChunkName: "DataPrepConnections" */ 'components/DataPrepConnections'),
+  loading: LoadingSVGCentered
+});
+const DataPrepHome = Loadable({
+  loader: () => import(/* webpackChunkName: "DataPrepHome" */ 'components/DataPrepHome'),
+  loading: LoadingSVGCentered
+});
+const RulesEngineHome = Loadable({
+  loader: () => import(/* webpackChunkName: "RulesEngineHome" */ 'components/RulesEngineHome'),
+  loading: LoadingSVGCentered
+});
+const DatasetDetailedView = Loadable({
+  loader: () => import(/* webpackChunkName: "DatasetDetailedView" */ 'components/DatasetDetailedView'),
+  loading: LoadingSVGCentered
+});
+const StreamDetailedView = Loadable({
+  loader: () => import(/* webpackChunkName: "StreamDetailedView" */ 'components/StreamDetailedView'),
+  loading: LoadingSVGCentered
+});
+const AppDetailedView = Loadable({
+  loader: () => import(/* webpackChunkName: "AppDetailedView" */ 'components/AppDetailedView'),
+  loading: LoadingSVGCentered
+});
+const Experiments = Loadable({
+  loader: () => import(/* webpackChunkName: "Experiments" */ 'components/Experiments'),
+  loading: LoadingSVGCentered
+});
+const ExperimentsCreateView = Loadable({
+  loader: () => import(/* webpackChunkName: "ExperimentsCreateView" */ 'components/Experiments/CreateView'),
+  loading: LoadingSVGCentered
+});
+
+const ExperimentDetailedView = Loadable({
+  loader: () => import(/* webpackChunkName: "ExperimentsDetailedView" */ 'components/Experiments/DetailedView'),
+  loading: LoadingSVGCentered
+});
+
+export default class Home extends Component {
+  componentWillMount() {
+    NamespaceStore.dispatch({
+      type: NamespaceActions.selectNamespace,
+      payload: {
+        selectedNamespace: this.props.match.params.namespace
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/ns/:namespace" component={EntityListView} />
+          <Route path="/ns/:namespace/apps/:appId" component={AppDetailedView} />
+          <Route path="/ns/:namespace/datasets/:datasetId" component={DatasetDetailedView} />
+          <Route path="/ns/:namespace/streams/:streamId" component={StreamDetailedView} />
+          <Route exact path="/ns/:namespace/rulesengine" component={RulesEngineHome} />
+          <Route exact path="/ns/:namespace/dataprep" component={DataPrepHome} />
+          <Route exact path="/ns/:namespace/dataprep/:workspaceId" component={DataPrepHome} />
+          <Route path="/ns/:namespace/databasebrowser" render={() => {
+            setActiveBrowser({ name: 'database' });
+            setDatabaseProperties({
+              properties: {
+                connectionString: 'jdbc:mysql://localhost:3306/test',
+                userName: 'root',
+                password: 'root',
+                databasename: 'test'
+              }
+            });
+            return (
+              <DataPrepBrowser />
+            );
+          }} />
+          <Route path="/ns/:namespace/connections" component={DataPrepConnections} />
+          <Route exact path="/ns/:namespace/experiments/create" component={ExperimentsCreateView} />
+          <Route exact path="/ns/:namespace/experiments" component={Experiments} />
+          <Route exact path="/ns/:namespace/experiments/:experimentId" component={ExperimentDetailedView} />
+          <Route component={Page404} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  params: PropTypes.shape({
+    namespace : PropTypes.string
+  }),
+  match: PropTypes.object
+};
