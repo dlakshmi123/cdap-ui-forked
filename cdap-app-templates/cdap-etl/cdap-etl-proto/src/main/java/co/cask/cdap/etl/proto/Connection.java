06ec991b84c592e67a55dba7de9214e2ca3e230b
dlakshmi123 <114049829+dlakshmi123@users.noreply.github.com>
2023-02-16 12:46:07 +0530

    Initialize bugfix/ui-pipelines-nodes

---
+++ cdap-app-templates/cdap-etl/cdap-etl-proto/src/main/java/co/cask/cdap/etl/proto/Connection.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.etl.proto;
+
+import co.cask.cdap.etl.proto.v2.ETLStage;
+
+import java.util.Objects;
+import javax.annotation.Nullable;
+
+/**
+ * Represents a connection between two {@link ETLStage}
+ */
+public class Connection {
+  private final String from;
+  private final String to;
+  private final String port;
+
+  public Connection(String from, String to) {
+    this(from, to, null);
+  }
+
+  public Connection(String from, String to, @Nullable String port) {
+    this.from = from;
+    this.to = to;
+    this.port = port;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  @Nullable
+  public String getPort() {
+    return port;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Connection that = (Connection) o;
+
+    return Objects.equals(from, that.from) && Objects.equals(to, that.to) && Objects.equals(port, that.port);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(from, to, port);
+  }
+
+  @Override
+  public String toString() {
+    return "Connection{" +
+      "from='" + from + '\'' +
+      ", to='" + to + '\'' +
+      ", port='" + port + '\'' +
+      '}';
+  }
+}
---
+++ cdap-ui/app/cdap/components/PipelineSummary/Store/PipelineSummaryActions.js
@@ -0,0 +1,210 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+
+import {MyProgramApi} from 'api/program';
+import PipelineSummaryStore, {PIPELINESSUMMARYACTIONS} from 'components/PipelineSummary/Store/PipelineSummaryStore';
+import {MyMetricApi} from 'api/metric';
+import {convertProgramToMetricParams} from 'services/program-api-converter';
+import {objectQuery} from 'services/helpers';
+import isNil from 'lodash/isNil';
+
+function setRuns(runs) {
+  PipelineSummaryStore.dispatch({
+    type: PIPELINESSUMMARYACTIONS.SETRUNS,
+    payload: {
+      runs: runs.map(run => ({
+        runid: run.runid,
+        start: run.start,
+        end: run.end,
+        duration: isNil(run.end) ? (Math.ceil(Date.now()/1000) - run.start) : (run.end - run.start),
+        status: run.status
+      }))
+    }
+  });
+}
+
+function fetchMetrics({namespaceId, appId, programType, programId}, metrics) {
+  let {runs} = PipelineSummaryStore.getState().pipelinerunssummary;
+  let postBody = {};
+  runs.forEach((run) => {
+    postBody[`qid_${run.runid}`] = {
+      tags: {
+        namespace: namespaceId,
+        app: appId,
+        [convertProgramToMetricParams(programType)]: programId,
+        run: run.runid
+      },
+      metrics,
+      timeRange: {
+        aggregate: 'true'
+      }
+    };
+  });
+  return MyMetricApi.query(null, postBody);
+}
+
+function updateMetrics(metrics, mapKey = 'metrics') {
+  let modMetrics = {};
+  Object
+    .keys(metrics)
+    .forEach(key => {
+      let runId = key.replace('qid_', '');
+      modMetrics[runId] = {[mapKey]: {}};
+      metrics[key]
+        .series
+        .forEach(metricObj => {
+          modMetrics[runId][mapKey][metricObj.metricName] = objectQuery(metricObj, 'data', 0, 'value');
+        });
+    });
+  let {runs} = PipelineSummaryStore.getState().pipelinerunssummary;
+  runs = runs.map(run => {
+    let runid = run.runid;
+    return Object.assign({}, run, modMetrics[runid]);
+  });
+  PipelineSummaryStore.dispatch({
+    type: PIPELINESSUMMARYACTIONS.SETRUNS,
+    payload: {
+      runs
+    }
+  });
+}
+
+function updateNodeMetrics(pipelineConfig) {
+  let {runs} = PipelineSummaryStore.getState().pipelinerunssummary;
+  const sourcePluginTypes = ['batchsource', 'realtimesource', 'streamingsource'];
+  const sinkPluginTypes = ['batchsink', 'realtimesink', 'sparksink'];
+
+  let nodesMap = {
+    sources: {},
+    sinks: {}
+  };
+  pipelineConfig.config.stages.forEach(stage => {
+    if (sourcePluginTypes.indexOf(stage.plugin.type) !== -1) {
+      nodesMap.sources[stage.name] = [];
+    }
+    if (sinkPluginTypes.indexOf(stage.plugin.type) !== -1) {
+      nodesMap.sinks[stage.name] = [];
+    }
+  });
+  /*
+    Sample node metrics:
+      {
+        "user.TPFSAvro.records.out": 40,
+        "user.TPFSAvro.records.in": 40,
+        "user.File.records.in": 40,
+        "user.File.records.out": 40,
+      }
+    Sample nodesMap:
+      {
+        sources: {
+          TPFSAvro: {}
+        },
+        sinks: {
+          File: {}
+        }
+      }
+  */
+  const getRecords = (run, nodesMap, type = '.records.in') => {
+    let cleanUpRegex = /\user.|\.records\.out|\.records\.in/gi;
+    let getNodeLabel = (node) => node.replace(cleanUpRegex, () => '');
+    let nodesCount = Object.keys(nodesMap);
+    let metricsCount = Object.keys(run.nodesMetrics);
+    let defaultMetrics = {};
+    Object.keys(nodesMap).map(n => defaultMetrics[`user.${n}${type}`] = 0);
+    let nodesWithMetrics = run.nodeMetrics;
+    if (!nodesCount.length || nodesCount.length!== metricsCount.length) {
+      nodesWithMetrics = Object.assign({}, defaultMetrics, run.nodesMetrics);
+    }
+    return Object
+      .keys(nodesWithMetrics)
+      .filter(node => node.indexOf(type) !== -1 && !isNil(nodesMap[getNodeLabel(node)]))
+      .map(node => ({
+        [getNodeLabel(node)]: run.nodesMetrics[node] || 0
+      }));
+  };
+  const addRunRecordTo = (map, node, run) => {
+    let nodeLabel = Object.keys(node).pop();
+    let runRecord = Object.assign({}, run, {
+      numberOfRecords: node[nodeLabel]
+    });
+    map[nodeLabel].push(runRecord);
+  };
+  runs.forEach(run => {
+    getRecords(run, nodesMap.sources)
+      .forEach(node => addRunRecordTo(nodesMap.sources, node, run));
+    getRecords(run, nodesMap.sinks, '.records.out')
+      .forEach(node => addRunRecordTo(nodesMap.sinks, node, run));
+  });
+  PipelineSummaryStore.dispatch({
+    type: PIPELINESSUMMARYACTIONS.SETNODESMETRICSMAP,
+    payload: {
+      nodesMap
+    }
+  });
+}
+
+function fetchSummary({namespaceId, appId, programType, programId, pipelineConfig, limit = -1, start = -1, end = -1}) {
+  let params = {
+    namespace: namespaceId,
+    appId,
+    programType,
+    programId
+  };
+  PipelineSummaryStore.dispatch({
+    type: PIPELINESSUMMARYACTIONS.SETLOADING,
+    payload: {
+      loading: true
+    }
+  });
+  const addProperty = (propertyValue, propertyName) => {
+    if (propertyValue === -1) {
+      return {};
+    }
+    return {[propertyName]: propertyValue};
+  };
+  params = Object.assign({}, params, addProperty(limit, 'limit'), addProperty(start, 'start'), addProperty(end, 'end'));
+  MyProgramApi
+    .runs(params)
+    .subscribe((runs) => {
+      setRuns(runs);
+      let logMetrics = [
+        'system.app.log.error',
+        'system.app.log.warn',
+        'system.app.log.info',
+        'system.app.log.debug',
+        'system.app.log.trace'
+      ];
+      let nodeMetrics = [];
+      pipelineConfig.config.stages.forEach(node => {
+        nodeMetrics = nodeMetrics.concat([`user.${node.name}.records.in`, `user.${node.name}.records.out`]);
+      });
+      PipelineSummaryStore.dispatch({
+        type: PIPELINESSUMMARYACTIONS.SETNODEMETRICSLOADING,
+        payload: {
+          nodemetricsloading: true
+        }
+      });
+      fetchMetrics(params, logMetrics)
+        .subscribe(logsMetrics => updateMetrics(logsMetrics, 'logsMetrics'));
+      fetchMetrics(params, nodeMetrics)
+        .subscribe(nodesMetrics => {
+          updateMetrics(nodesMetrics, 'nodesMetrics');
+          updateNodeMetrics(pipelineConfig);
+        });
+    });
+}
+
+export {fetchSummary};
